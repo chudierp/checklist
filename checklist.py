ae421a8671cb1743333f5a627a8566700533a02f
Chudier <chudierp@gmail.com>
2019-09-15 16:35:31 -0700

    Updated code for revision

--- checklist.py
+++ checklist.py
@@ -21,30 +21,47 @@ def list_all_items():
         index += 1
 
 def mark_completed(index):
-    update(index, "√" + checklist[item])
+    update(index, "√" + checklist[index])
     list_all_items()
 
 def select(function_code):
     # Create item
+
+
     if function_code == "C":
         input_item = user_input("Input item:")
         create(input_item)
 
     # Read item
     elif function_code == "R":
-        item_index = user_input("Index Number?")
+        item_index = user_input("Index Number")
 
         # Remember that item_index must actually exist or our program will crash.
         print(read(int(item_index)))
 
+    elif function_code == "D":
+        index = user_input("Index Number")
+        destroy(index)
+        list_all_items()
+
+    elif function_code == "U":
+        index = user_input("Index Number")
+        item = user_input("item")
+        update(int(index), item)
+        list_all_items()
 
     # Print all items
     elif function_code == "P":
         list_all_items()
 
+    elif function_code == "Q":
+        # This is where we want to stop our loop
+        return False
+
     # Catch all
     else:
         print("Unknown Option")
+    return True
 
 def user_input(prompt):
     # the input function will display a message in the terminal
@@ -79,5 +96,5 @@ test()
 running = True
 while running:
     selection = user_input(
-        "Press C to add to list, R to Read from list and P to display list")
-    select(selection)
+        "Press C to add to list, Q to stop loop, R to Read from list, D to remove items and P to display list")
+    running = select(selection)
